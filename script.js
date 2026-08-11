8b082f465cfe91164d591b750980298cb1e8ee9a
thi310721-boop <thi310721@gmail.com>
2026-08-11 09:31:38 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// เพิ่ม JavaScript สำหลับการโต้ตอบกับผู้โล้(Optional)
+
 document.addEventListener('DOMContentLoaded',function () {
-    console.log('Portfolio ของ Thidarat พร้อมแล้ว!');
+    console.log('Portfolio ของ Thidarat พร้อมแล้ว! '  ) ;
 }};        
